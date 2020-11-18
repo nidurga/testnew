fea3f1a98fe05f6408703c16166506a320a232e8
nidurga <nikhil.durga@copart.com>
2020-11-18 12:30:11 -0600

    Update add.py

--- add.py
+++ add.py
@@ -1,4 +1,4 @@
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+num1 = 20
+num2 = 45.8
 sum = float(num1) + float(num2)
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))

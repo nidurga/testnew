f5dc69ba5cf0c045cbbbbbcc68534f4aca29e864
nidurga <nikhil.durga@copart.com>
2020-12-09 16:22:13 -0600

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,8 +1,1 @@
-cron_file1_py = x y z 
-cron_file2_txt = 1 2 3
-cron_file3_java = A B C
-cron_file4_py = D V A
-cron_file5_py = D V A
-cron_file6_py = D V 1
-cron_file7_py = D V 1
-cron_file8_py = D V 1
+cron_file15_py = x y z 
